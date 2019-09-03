96c571e8f9a1ec7d06c91a232311ce931afc1eeb
Ronald-Crosby <olly_robinson@live.com>
2019-09-03 15:58:16 +0100

    enjoy test

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content : [
+        'index.html'
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+const cssnano = require('cssnano')
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss'),
-        require('cssnano')
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss, cssnano]
+            : []
     ]
 }
 
